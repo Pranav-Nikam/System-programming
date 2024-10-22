a38efeb58ef3de723fcdea5629bed02f0c4e9670
Pranav Nikam (Dambe) <154051249+Pranav-Nikam@users.noreply.github.com>
2024-10-22 13:31:00 +0530

    Assignment-6

---
+++ Assignment-6/input.c
@@ -0,0 +1,11 @@
+int main() 
+{
+    int p = 15;
+    int n = 22; 
+    // ignore this line.
+    int sum = p + n; // sum
+    return 0;
+
+    // int 98_number = 20;   --> Error
+    // int num@ber = 22;
+}
